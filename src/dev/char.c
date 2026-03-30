24a3ea60535c3e69867574d65c5f78d303ca37aa
Cory Borek <agentdid127cb@aol.com>
2026-03-30 09:05:28 -0400

    Add Character device

--- src/dev/char.c
+++ src/dev/char.c
@@ -1,4 +1,24 @@
 #include <dev/char.h>
+#include <bus.h>
 #include <stdio.h>
+
+BUS_DEV char_device;
+
+
+char read_val;
+uint8_t char_read(size_t offset) {
+    scanf("%c", &read_val);
+    return read_val;
+}
+
+void char_write(size_t offset, uint8_t val) {
+    printf("%c", val);
+}
+
 void char_init() {
+    char_device.address = DEV_CHAR_ADDR;
+    char_device.length = 1;
+    char_device.read_char = char_read;
+    char_device.write_char = char_write;
+    register_bus_device(&char_device);
 }
