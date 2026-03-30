beb6ca3bd41d9b4f8bfd1023c5cdffe51caeb847
Cory Borek <agentdid127cb@aol.com>
2026-03-30 09:05:14 -0400

    Add ROM device

--- src/dev/rom.c
+++ src/dev/rom.c
@@ -1,7 +1,45 @@
 #include <dev/rom.h>
 #include <bus.h>
-void rom_init() {
-void write_rom_char(data_t addr, uint8_t data) {
+#include <stdio.h>
+#include <util.h>
+
+unsigned char rom[ROM_SIZE];
+
+BUS_DEV rom_device;
+
+void write_rom_dummy(size_t offset, uint8_t val) {
+    return;
 }
+
+uint8_t read_rom_char(size_t offset) {
+    if (offset >= 0 && offset < ROM_SIZE) {
+        return rom[offset];
+    }
+    return 0;
+}
+
+void rom_init() {
+    rom_device.address = ROM_ADDR;
+    rom_device.length = ROM_SIZE;
+    rom_device.read_char = read_rom_char;
+    rom_device.write_char = write_rom_dummy;
+    register_bus_device(&rom_device);
+    log_message(LOG_DEBUG, "Initialized ROM.\n");
+}
+
+union int_to_char {
+  data_t ic_val;
+  uint8_t ic_char[sizeof(data_t)];
+};
+
+void write_rom_char(data_t addr, uint8_t data) {
+    rom[addr] = data;
+}
+
 void write_rom(data_t addr, instr_t data) {
+  union int_to_char conv;
+  conv.ic_val = data;
+  for (int i = 0; i < sizeof(instr_t); i++) {
+    write_rom_char(addr + i, conv.ic_char[i]);
+  }
 }
